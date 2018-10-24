80439f2af856a0234de956115a5ec06869985c6b
sjohner <sjohner@posteo.ch>
2018-10-24 12:23:36 +0200

    Changed title and desc of controls

--- azure_profile/controls/check_rg_grcpool-prod-rg.rb
+++ azure_profile/controls/check_rg_grcpool-prod-rg.rb
@@ -1,7 +1,7 @@
 control 'exists_resourcegroup' do
     impact 1.0
-    title "Resource Group exists"
-    desc "A resource group grcpool-prod-rg should exist."
+    title "Check resource group grcpool-prod-rg"
+    desc "Check if resource group grcpool-prod-rg is present"
     describe azurerm_resource_groups.where('name' => 'grcpool-prod-rg') do
         it { should exist }
     end
